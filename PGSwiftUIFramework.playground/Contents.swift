0a7d0b6ca0dc77a7b1020d388d47ac10f774a862
cod3breaker <ravisherath@gmail.com>
2020-06-07 19:13:01 +1000

    Reversing with opaque type

--- PGSwiftUIFramework.playground/Contents.swift
+++ PGSwiftUIFramework.playground/Contents.swift
@@ -21,3 +21,28 @@ func reverseit(mylist: [Int]) -> ReversedCollection<Array<Int>> {
 let reversedList = reverseit(mylist: listToReverse)
 print(Array(reversedList))
 
+
+// Reversing a list of Strings
+var listOfStringsToReverse: [String] = ["Aa", "Bb", "Cc","Dd","Ee","Ff"]
+
+func reverseit(mylist: [String]) -> ReversedCollection<Array<String>> {
+    let reversed = mylist.reversed()
+    return reversed
+}
+
+let reversedStringsList = reverseit(mylist: listOfStringsToReverse)
+print(Array(reversedStringsList))
+
+
+// With Opaque types
+
+func reverseitWithOpaque(mylist: [Int]) -> some Collection {
+    let reversed = mylist.reversed()
+    return reversed
+}
+
+let reversedIntList = reverseitWithOpaque(mylist: listToReverse)
+print(Array(reversedIntList))
+
+
+
