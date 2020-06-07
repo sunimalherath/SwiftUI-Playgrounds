1707780f7ee6c2ffce666d45f75e676430144dbd
cod3breaker <ravisherath@gmail.com>
2020-06-07 18:58:38 +1000

    reversing a Integer array

--- PGSwiftUIFramework.playground/Contents.swift
+++ PGSwiftUIFramework.playground/Contents.swift
@@ -1,3 +1,23 @@
-import UIKit
+import SwiftUI
 
-var str = "Hello, playground"
+/**
+ Opaque Types:
+ 
+ These types are declared with "some" keyword followed by the name of the protocol it confirms to.
+ 
+ e.g: var body: some View
+        body conforms to the View protocol
+ */
+
+
+// Reversing a list of Integers
+var listToReverse: [Int] = [1,2,3,4,5,6]
+
+func reverseit(mylist: [Int]) -> ReversedCollection<Array<Int>> {
+    let reversed = mylist.reversed()
+    return reversed
+}
+
+let reversedList = reverseit(mylist: listToReverse)
+print(Array(reversedList))
+
